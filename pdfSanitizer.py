ca84262b741e3bf27490bc4362eeab41fd61ab0d
1995hyz <1995hyz@gmail.com>
2018-05-23 22:04:49 -0400

    Full Version without Comments

--- pdfSanitizer.py
+++ pdfSanitizer.py
@@ -1,17 +1,27 @@
 from wand.image import Image
 from PIL import Image as Image2
+import io
+import PyPDF2
 import sys
-
-def converter(inputFile):
-	with Image(filename=inputFile, resolution=300) as img:
-		img.format='jpeg'
-		img.save(filename='tempJPG')
-		returner('tempJPG')
-
-def returner(inputFile):
-	im=Image2.open(inputFile)
-	im.save('OutFile',"PDF",resolution=300)
 
 if __name__=="__main__":
 	line=raw_input('Please Type in the Input File Name: ')
-	converter(line)
+	targetPDF=PyPDF2.PdfFileReader(line)
+	merger=PyPDF2.PdfFileMerger()
+	for i in xrange(targetPDF.getNumPages()):
+		splitPDF=PyPDF2.PdfFileWriter()
+		splitPDF.addPage(targetPDF.getPage(i))
+		splitPdfBuffer=io.BytesIO()
+		imgBuffer=io.BytesIO()
+		outPdfBuffer2=io.BytesIO()
+		splitPDF.write(splitPdfBuffer)
+		splitPdfBuffer.seek(0)
+		img=Image(file=splitPdfBuffer,resolution=500)
+		img.format='jpeg'
+		img.save(imgBuffer)
+		imgBuffer.seek(0)
+		im=Image2.open(imgBuffer)
+		im.save(outPdfBuffer2,"PDF",resolution=500)
+		outPdfBuffer2.seek(0)
+		merger.append(outPdfBuffer2)
+	merger.write(line)
