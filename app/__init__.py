bdab808743c034da979a28686c2f5c942c6aaf2b
1995hyz <1995hyz@gmail.com>
2018-06-30 21:18:26 -0400

    Working on Login Validation

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import login, model
+from app import routes, model
