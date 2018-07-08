b6d9d4b8a55bf6cf222562b7547b0390160dc0a7
1995hyz <1995hyz@gmail.com>
2018-07-08 15:30:30 -0400

    Fix Bugs, Add Bootstrap

--- app/routes.py
+++ app/routes.py
@@ -3,21 +3,20 @@ from app.forms import LoginForm, RegistrationForm
 from app import app, db
 from flask_login import current_user , login_user, logout_user, login_required
 from app.model import User
-from app.index import hello_world
 from werkzeug.urls import url_parse
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 @login_required
-def index():
-	return render_template('index.html')
+def home():
+	return render_template('home.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
-		return redirect('/index')
+		return redirect('/home')
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
@@ -27,7 +26,7 @@ def login():
 		login_user(user, remember=form.remember_me.data)
 		next_page = request.args.get('next')
 		if not next_page or url_parse(next_page).netloc != '':
-			next_page = '/index'
+			next_page = '/login'
 		return redirect(next_page)
 	return render_template('login.html', form=form)
 
@@ -35,13 +34,13 @@ def login():
 @app.route('/logout')
 def logout():
 	logout_user()
-	return redirect('/index')
+	return redirect('/login')
 
 
 @app.route('/register', methods=['GET','POST'])
 def register():
 	if current_user.is_authenticated:
-		return redirect('/index')
+		return redirect('/home')
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		user = User(username=form.username.data)
