3ddcf0482956f9af4f1778f2c33f829e99aadbae
1995hyz <1995hyz@gmail.com>
2018-07-14 17:28:53 -0400

    Run under Virtual

---
+++ venv/app/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.model import User
+
+class LoginForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password_check = PasswordField(
+	'Re-enter Password', validators=[DataRequired(), EqualTo('password')])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+	first_name = StringField('First Name', validators=[DataRequired()])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise(ValidationError('The email address has existed.'))
+
+class ProfileForm(FlaskForm):
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+
+
+class ToSupervisorForm(FlaskForm):
+	username = StringField('Change To Supervisor', validators=[DataRequired(),
+	 Email()])
+	submit1 = SubmitField('Submit')
+
+
+class ToOperatorForm(FlaskForm):
+	username = StringField('Change To Operator', validators=[DataRequired(),
+	 Email()])
+	submit2 = SubmitField('Submit')
---
+++ venv/app/routes.py
@@ -0,0 +1,146 @@
+from flask import render_template, request, redirect, flash
+from app.forms import LoginForm, RegistrationForm
+from app.forms import ToSupervisorForm, ToOperatorForm
+from app import app, db
+from flask_login import current_user , login_user, logout_user, login_required
+from app.model import User, Timeslot
+from werkzeug.urls import url_parse
+
+
+@app.route('/')
+@app.route('/home')
+@login_required
+def home():
+	return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect('/home')
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect('/login')
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = '/login'
+		return redirect(next_page)
+	return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect('/login')
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect('/home')
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data,
+		 			first_name=form.first_name.data,
+		 			last_name=form.last_name.data,
+					privilege=False)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('You have completed the registration.')
+		return redirect('/login')
+	return render_template('register.html', form=form)
+
+
+@app.route('/profile')
+@login_required
+def profile():
+	#This function will return the user profile. (Being developed later)
+	return render_template('profile.html', user=current_user)
+
+
+@app.route('/admin')
+@login_required
+def admin_page():
+	if not current_user.privilege:
+		flash('Only Admin can view the page!')
+		return redirect('/home')
+	return render_template('admin.html')
+
+
+@app.route('/external')
+def external_links():
+	'''This function will redirect to some exteranl links
+	such as ccwiki and CUPS'''
+	return render_template('external.html')
+
+
+@app.route('/privilege', methods=['GET', 'POST'])
+@login_required
+def privilege_page():
+	if not current_user.privilege:
+		flash('Only Admin can view the page!')
+		return redirect('/home')
+	form1 = ToSupervisorForm()
+	form2 = ToOperatorForm()
+	if form1.submit1.data and form1.validate_on_submit():
+		user = User.query.filter_by(username=form1.username.data).first()
+		if user:
+			user.privilege = True
+			db.session.commit()
+			flash(user.username + ' has become Supervisor')
+			return redirect('/privilege')
+		else:
+			flash('Invalid Email Account')
+			return redirect('/privilege')
+	if form2.submit2.data and form2.validate_on_submit():
+		user = User.query.filter_by(username=form2.username.data).first()
+		if user:
+			user.privilege = False
+			db.session.commit()
+			flash(user.username + 'has become Operator')
+			return redirect('/privilege')
+		else:
+			flash('Invalid Email Account')
+			return redirect('/privilege')
+	return render_template('privilege.html', user=User.query.all(),
+							form2=form2, form1=form1)
+
+
+@app.route('/schedule', methods=['GET', 'POST'])
+@login_required
+def schedule_page():
+	if not current_user.privilege:
+		flash('Only Admin can view the page!')
+		return redirect('/home')
+	return render_template('/schedule.html')
+
+
+@app.route('/week_schedule_m', methods=['GET', 'POST'])
+@login_required
+def week_schedule_monday():
+	if not current_user.privilege:
+		flash('Only Admin can view the page!')
+		return redirect('/home')
+	hours = ['8', '9', '10']
+	orders = ['0', '1', '2', '3']
+	slots = Timeslot.query.filter_by(week=0)
+	slot_dic = {}
+	for slot in slots:
+		key = str(slot.time) + '/' + str(slot.index)
+		slot_dic[key] = slot.open
+	return render_template('/week_schedule_m.html',
+	 						hours=hours, orders=orders, selectable=slot_dic)
+
+
+@app.route('/changed_week', methods=['GET', 'POST'])
+@login_required
+def changed_week():
+	if not current_user.privilege:
+		flash('Only Admin can view the page!')
+		return redirect('/home')
+	return render_template('/changed_week.html')
