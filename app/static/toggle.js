bac61e7dac877a9abc6f9b19e7b468403d0506c8
1995hyz <1995hyz@gmail.com>
2018-07-19 21:20:29 -0400

    Remove app

--- app/static/toggle.js
+++
@@ -1,7 +0,0 @@
-function toggle(checkboxID, toggleName) {
-     var checkbox = document.getElementById(checkboxID);
-     var toggle = document.getElementsByName(toggleName);
-     for(var i=0;i<toggle.length;i++){
-        updateToggle = checkbox.checked ? toggle[i].disabled=false : toggle[i].disabled=true;
-     }
-}
